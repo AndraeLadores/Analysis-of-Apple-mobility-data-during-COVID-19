d535be3cb5eb334b267c86af9e48939d83a12320
Andrae Ladores <vcladores@dons.usfca.edu>
2021-03-14 16:15:38 -0700

    Separate main code and function code into two codes

--- code/03_long_data_in_california.R
+++ code/03_long_data_in_california.R
@@ -1,26 +1,12 @@
 # We want to turn California subsetted wide data into long data
 # We also want to fix the date
 
-# read in file
-california_long_data <- readr::read_csv(
-  "output/applemobilitytrends-2021-02-20_California.csv")
+# Source in the function
+source("code/functions/convert_subset_data_to_long.R")
 
-# load in tidyr
-library("tidyr")
-library("readr")
-
-
-# Make table long instead of wide, try to fix it/make it show on an object
-fixed_california_data <- california_long_data %>%
-tidyr::pivot_longer(
-  cols = starts_with("X202"),
-  names_to = "Date",
-  names_prefix = "X",
-  values_to = "Frequency"
-)
-
-readr::write_csv(fixed_california_data,
-                 file = paste0("output/",
-                               "california_mobility_data_long.csv"
-                               ))
-
+# Run the function
+convert_long_state_data(input_wide_subsetted_data =
+                                  paste0("output/",
+                                         "subsetted_states_wide",
+                                         "/applemobilitytrends",
+                                         "-2021-02-20_Alaska.csv"))
