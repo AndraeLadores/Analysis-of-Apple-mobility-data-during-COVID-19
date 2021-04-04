4898c9134bc85be5eab0ece0e78fa266692128a8
AndraeLadores <vcladores@dons.usfca.edu>
2021-04-04 21:42:53 +0000

    Create bash prompt to read sequences

---
+++ code/process_sequences.sh
@@ -0,0 +1,7 @@
+# Purpose of this bash script is to take the path to a single compressed fasta file as a command-line 
+# argument
+
+# Created on April 4, 2021
+# Andrae Ladores - vcladores@dons.usfca.edu
+
+
