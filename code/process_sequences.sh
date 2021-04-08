6d305bf154c21460e9c338cebf7339968d2017c3
AndraeLadores <vcladores@dons.usfca.edu>
2021-04-08 02:45:53 +0000

    Updated bash code to do required work

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-# Purpose of this bash script is to take the path to a single compressed fasta file as a command-line 
+# Purpose of this bash script is to take the path to a single compressed fasta file as a command-line
 # argument
 
 # Created on April 4, 2021
 # Andrae Ladores - vcladores@dons.usfca.edu
 
 # 1 Count total number of sequences in the file
-# 2 Tally up the number of SARS-CoV-2 sequences in the 
+# 2 Tally up the number of SARS-CoV-2 sequences in the
 # dataset from each country and sort this output from largest to smallest
 
 
 # 1: Use zcat to analyze the compressed fasta file to see how we can specifically filter
 # only the sequence
-
-# 'zcat /blast-db/sars-cov-2-seq-data$/2021...etc | head -n 1000?'
-
-# Then we can use this code to ensure that after "complete genome" the sequences are stated
-
-# `zgrep -A 1 "complete genome" /blast-db/sars-cov2-seq/data$/2021...etc`
- 
-# Find a way to somehow only capture and then count ONLY the sequences. This is probably
-# a regular expression along with pipes!
-# Maybe use zgrep -v to find filter out anything but the sequences!
-	##`zgrep -v "[^A-Z] ...etc`
-	##`zgrep -v "genome$"
-	##`zgrep -v "genome$" | wc -l` This seems right, probably put defensive programming here1
-
-# Why are there "N" in the sequences?
 
 # Using zcat to analyze the compressed fasta file, just to take a peek.
 cd /
@@ -38,27 +23,32 @@ cd /blast-db/sars-cov-2-seq-data/
 echo "/blast-db/sars-cov-2-seq-data/"
 echo "cd to go into blast-db folder"
 
-zcat 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | head
-echo "zcat 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz"
-echo "This is zcat"
+if [ $# = 0 ]
+then
+	echo "Please input necessary file as an argument"
+	exit 1
+fi
 
-#zgrep -v "genome$" 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | wc -l
-#echo "This is how many sequences there are in total"
+# zcat 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | head
+# echo "This is zcat results"
 
 # Build up on zgrep to somehow get only the country and their total count 
-#Find a way to get only the country name!
+# Find a way to get only the country name!
 
 #echo "This is China numbers"
 #zgrep "China" 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | zgrep -v "genome$" |\
-#wc -l | sed -E 's/(.*)/China \1/' 
+#wc -l | sed -E 's/(.*)/China \1/'
 
 # This gives the same output
 # zgrep "China" 2021...etc | bioawk -c fastx '{print $seq}' | wc -l
 
 
 # figure out how to report and list all the countries with their given number of sequences
-country=(China, Iran)
-zgrep $country 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | bioawk -c fastx '{print $seq}' | wc -l | sed -E 's/(.*)/'$country' \1/'
 
+##zgrep "USA" 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | bioawk -c fastx '{print $seq}' | wc -l | sed -E 's/(.*)/USA \1/'
 
-#bioawk -c fastx '{/country/ print $seq}' 2021... | wc -l > country_sequences.txt
+##bioawk -c fastx '{print $comment,$seq}' 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | sed -E '/s(|.*|{3})/\1
+
+#bioawk -c fastx '{print $comment,length($seq)}' 2021-04-05_ncbi_sars_cov_2_sequences.fasta.gz | awk '{print $14}' | head -n 2
+
+bioawk -c fastx '{print length($seq),$comment}' "$1" | awk '{split($0,a,"|"); print a[5]}' | sort | uniq -c | sort -k1,1nr
