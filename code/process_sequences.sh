4bc6226f29299b1da53a36ae7f08e672ff7c96d8
AndraeLadores <vcladores@dons.usfca.edu>
2021-04-14 05:57:56 +0000

    Fix minor issue

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -16,15 +16,10 @@ then
 	exit 1
 fi
 
-if [ $# -gt 0 ]
-then
-
-	echo -e "${GRN}Success, please wait${NC}"
 
 # Tally up number of sequences from each country and tally from biggest to smallest
 bioawk -c fastx '{print length($seq),$comment}' "$1" | awk '{split($0,a,"|"); print a[5]}' | sort | uniq -c | sort -k1,1nr
-	exit 1
-fi
+
 
 
 # Tally up number of sequences from each country and tally from biggest to smallest
